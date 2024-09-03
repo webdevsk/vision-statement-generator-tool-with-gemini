508c3eb8c08081ed500b71e0d281f5dbf7f352fe
webdevsk <72684302+webdevsk@users.noreply.github.com>
2024-09-03 19:32:19 +0600

    Added tailwind base stuff

--- src/app/clientapp.js
+++ src/app/clientapp.js
@@ -4,17 +4,12 @@ import { getGeminiResponse } from "@/actions/get-gemini-response"
 import { useState } from "react"
 
 export function ClientApp() {
-    const [response, setResponse] = useState("")
-    const [prompt, setPrompt] = useState("")
-    console.log(prompt)
     async function getResponse() {
         const res = await getGeminiResponse({ prompt: prompt })
         setResponse(res.data)
     }
 
     return <>
-        <input type="text" value={prompt} onChange={(e) => setPrompt(e.currentTarget.value)} />
-        <button disabled={!prompt} onClick={getResponse}>Submit</button>
-        <p>{response}</p>
+
     </>
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,4 +1,62 @@
 /** @type {import('tailwindcss').Config} */
+import plugin from "tailwindcss/plugin"
+
+const fontStyles = {
+  ".variant-header": {
+    "font-size": "60px",
+    "font-weight": "700",
+    "line-height": "2",
+    // for responsive
+    // '@media (max-width: 500px)': {
+    //   "font-size": "40px",
+    // },
+    // for states
+    // '&:hover': {
+    //   "font-weight": "900",
+    // },
+  },
+  ".variant-h1": {
+    "font-size": "46px",
+    "font-weight": "600",
+    "line-height": "2",
+  },
+  ".variant-h2": {
+    "font-size": "38px",
+    "font-weight": "600",
+    "line-height": "2",
+  },
+  ".variant-h3": {
+    "font-size": "32px",
+    "font-weight": "600",
+    "line-height": "2",
+  },
+  ".variant-h4": {
+    "font-size": "26px",
+    "font-weight": "500",
+    "line-height": "1.5",
+  },
+  ".variant-h5": {
+    "font-size": "20px",
+    "font-weight": "500",
+    "line-height": "1.5",
+  },
+  ".variant-h6": {
+    "font-size": "16px",
+    "font-weight": "500",
+    "line-height": "1.5",
+  },
+  ".variant-p": {
+    "font-size": "16px",
+    "font-weight": "400",
+    "line-height": "1.5",
+  },
+  ".variant-small": {
+    "font-size": "14px",
+    "font-weight": "500",
+    "line-height": "1.5",
+  },
+}
+
 module.exports = {
   content: [
     "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
@@ -6,17 +64,42 @@ module.exports = {
     "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
-    extend: {
-      backgroundImage: {
-        "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
-        "gradient-conic":
-          "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
+    data: {
+      active: 'active="true"',
+      pending: 'pending="true"',
+      current: 'current="true"',
+    },
+    container: {
+      center: true,
+      padding: {
+        DEFAULT: "1rem",
+        sm: "2rem",
       },
+    },
+    screens: {
+      sm: "640px",
+      md: "768px",
+      lg: "1024px",
+      xl: "1280px",
+      "2xl": "1600px",
+    },
+    extend: {
       fontFamily: {
         sans: ['var(--font-geist-sans)'],
         mono: ['var(--font-geist-mono)'],
       },
     },
   },
-  plugins: [],
+  plugins: [
+    plugin(({ addComponents }) => {
+      addComponents(fontStyles)
+      addComponents({
+        ".container-fluid": {
+          maxWidth: "120rem",
+          marginInline: "auto",
+          paddingInline: "0",
+        },
+      })
+    }),
+  ],
 }
